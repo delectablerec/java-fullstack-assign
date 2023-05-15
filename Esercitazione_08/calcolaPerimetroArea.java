595c0912c1b001bb693165c7963ce1f0a6956631
delectablerec <info@delectablerecords.com>
2023-05-15 10:50:20 +0200

    02 inserita variabile lato per calcoli

--- Esercitazione_08/calcolaPerimetroArea.java
+++ Esercitazione_08/calcolaPerimetroArea.java
@@ -7,6 +7,7 @@ public class calcolaPerimetroArea // deve chiamarsi come il nome del file .java
     public static void main(String[] args)
     {
         // le istruzioni vanno scritte all'interno del metodo main
+        int lato; // questo sarà il parametro che utilizzerò per i calcoli
 
     }
 }
