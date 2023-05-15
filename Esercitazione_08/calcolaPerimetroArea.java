d78df5df15bfe4af513a1a082e580747e718c6ab
delectablerec <info@delectablerecords.com>
2023-05-15 10:41:08 +0200

    01 Creazione classi e metodi

---
+++ Esercitazione_08/calcolaPerimetroArea.java
@@ -0,0 +1,16 @@
+/*
+titolo dell'esercitazione e relativa spiegazione
+*/
+
+public class calcolaPerimetroArea // deve chiamarsi come il nome del file .java
+{
+    public static void main(String[] args)
+    {
+        // le istruzioni vanno scritte all'interno del metodo main
+
+    }
+}
+
+/*
+note per i collaboratori
+*/
