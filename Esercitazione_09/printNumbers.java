92f7ef37e16e8dd6284edcbf82c7fad135f29e3a
delectablerec <info@delectablerecords.com>
2023-05-15 15:51:26 +0200

    upload es 09

---
+++ Esercitazione_09/printNumbers.java
@@ -0,0 +1,15 @@
+public class printNumbers
+{
+    public static void main(String[] args)
+    {
+        for (int i = 1; i <= 10; i++)
+        // for identifica il tipo di ciclo che si verifica per un numero predeterminato di volte
+        // quante volte? Quanto è il valore che assume i
+        // i inizialmente è 1
+        // con il punto e virgola posso assegnare un altro valore ad i in questo caso 10 che rappresenta il numero massimo di cicli
+        // ad ogni ciclo i aumenta incrementalmente di 1 unità (perchè c'è ++)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Esercitazione_10/forEach.java
@@ -0,0 +1,12 @@
+public class forEach
+{
+    public static void main(String[] args)
+    {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+        for (int num : numbers)
+        {
+            int dim = numbers.length; // la classe array ha un metodo che descrive la quantità totale degli elementi
+            System.out.println("numero "+ num+" di "+dim+ " elementi");
+        }
+    }
+}
