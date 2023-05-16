c1bb011702830bc94bf007b026a05181dcdf8cc0
delectablerec <info@delectablerecords.com>
2023-05-16 11:24:07 +0200

    esercitazioni 10 ed 11

---
+++ Esercitazione_11/arrayStr.java
@@ -0,0 +1,11 @@
+public class arrayStr
+{
+    public static void main(String[] args)
+    {
+    String[] words = {"uno", "due", "tre", "quattro", "cinque"};
+        for (String word : words)
+        {
+            System.out.println(word);
+        }
+    }
+}
