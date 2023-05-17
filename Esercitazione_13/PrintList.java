2f6567a167812093d21c07baff5ccd6f39c8271e
delectablerec <info@delectablerecords.com>
2023-05-17 10:01:22 +0200

    vari metodi di classList

---
+++ Esercitazione_13/PrintList.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintList
+{
+    public static void main(String[] args)
+    {
+        List<String> fruits = new ArrayList<String>();
+        fruits.add("Mela"); // il metodo add della classe aggiunge un elemento
+        fruits.add("Banana");
+        fruits.add("Arancia");
+        // fruits.clear(); // il metodo per pulire
+        // fruits.remove(0); // il metodo per rimuovere un elemento specifico
+        // fruits.size();
+        fruits.isEmpty();
+        for (String fruit : fruits)
+        {
+            System.out.println(fruit);
+            // System.out.println(fruits.size());
+            // System.out.println(fruits.isEmpty());
+        }
+         System.out.println(fruits.isEmpty());
+    }
+}
